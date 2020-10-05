4a14674dc15166989a9c99e01918ccd47cbcd267
iqraidzharul12 <iqra.idzharul.haq@gmail.com>
2020-10-05 21:56:32 +0700

    routing

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,6 +8,11 @@ import testLab from "./testLab";
 import testLabEvidence from "./testLabEvidence";
 import programType from "./programType";
 import fileUpload from "./fileUpload";
+import pharmacy from "./pharmacy";
+import price from "./price";
+import program from "./program";
+import programEvidence from "./programEvidence";
+import voucher from "./voucher";
 
 const routes = Router();
 
@@ -20,5 +25,10 @@ routes.use("/testLab", testLab);
 routes.use("/testLabEvidence", testLabEvidence);
 routes.use("/programType", programType);
 routes.use("/upload", fileUpload);
+routes.use("/pharmacy", pharmacy);
+routes.use("/price", price);
+routes.use("/program", program);
+routes.use("/programEvidence", programEvidence);
+routes.use("/voucher", voucher);
 
 export default routes;
