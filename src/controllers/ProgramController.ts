0849617c2b676130e18004688f69dcc55e78af91
iqraidzharul12 <iqra.idzharul.haq@gmail.com>
2020-11-16 09:25:44 +0700

    program price

--- src/controllers/ProgramController.ts
+++ src/controllers/ProgramController.ts
@@ -31,6 +31,7 @@ class ProgramController {
         "pharmacy",
         "programEvidences",
         "testLab",
+        "price",
       ],
     });
 
@@ -50,6 +51,7 @@ class ProgramController {
         "pharmacy",
         "programEvidences",
         "testLab",
+        "price",
       ],
     });
 
@@ -75,6 +77,7 @@ class ProgramController {
           "pharmacy",
           "programEvidences",
           "testLab",
+          "price",
         ],
       });
       try {
